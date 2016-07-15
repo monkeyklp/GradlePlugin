06d59f5bb8540816c2842f9e502e0a328879d25c
monkey <klp13115@ly.com>
2016-07-15 09:05:25 +0800

    fix bug

--- buildSrc/src/main/groovy/com/klp/GreetingPlugin.groovy
+++ buildSrc/src/main/groovy/com/klp/GreetingPlugin.groovy
@@ -2,10 +2,10 @@ package com.klp
 
 import org.gradle.api.Plugin
 import org.gradle.api.Project
-import org.gradle.api.tasks.Copy;
 
 class GreetingPlugin implements Plugin<Project> {
     GreetingPluginExtension klpExt;
+    File originalOutputFile;
 
     void apply(Project project) {
         //演示添加外部属性
@@ -13,6 +13,8 @@ class GreetingPlugin implements Plugin<Project> {
         project.task('hello') << {
             println "${project.klp.message} from ${project.klp.greeter}"
         }
+
+
         project.afterEvaluate {//gradle建立完task的有向图
             //清除自定义的输出文件夹
             def cleanCustomOutputFileTask = project.task("cleanCustomOutputFile") << {
@@ -28,30 +30,18 @@ class GreetingPlugin implements Plugin<Project> {
                 }
             }
             //建立依赖
-            project.getTasksByName("clean", true).each {
+            project.getTasksByName("clean", true) << {
                 it.dependsOn cleanCustomOutputFileTask
                 println "clean custom out put file"
             }
 
 //
             project.android.applicationVariants.all { variant ->
-                def File originalOutputFile = variant.outputs[0].outputFile;// 原路径
-                def File customOutputFile = klpExt.outPutFile;//自定义的路径
-                println originalOutputFile.absoluteFile
-                println customOutputFile.absoluteFile
 
-                def copyChangeTask = project.task("copyChange${variant.name.capitalize()}", type: Copy) << {
-                    from originalOutputFile.absoluteFile
-                    into customOutputFile.absoluteFile
-                    println "copyChangeTask is runing"
-                    println originalOutputFile.absoluteFile.exists()
-                }
-
-                project.tasks.getByName("assemble"){
-                    it.doLast{
-                        println "do last"
-                        copyChangeTask
-                    }
+                originalOutputFile = variant.outputs[0].outputFile;// 原路径
+                println originalOutputFile.absolutePath
+                variant.outputs.each { output ->
+                    output.outputFile = new File(klpExt.outPutFile, "custom.apk")
                 }
             }
         }
